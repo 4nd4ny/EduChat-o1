1441a0d92db9ce22f3a62b38c20af1793514291c
4nd4ny <christian.andany@gmail.com>
2024-10-14 21:17:42 +0200

    Nettoyage : contexte plus utilisé

--- src/chat/ChatMessage.tsx
+++ src/chat/ChatMessage.tsx
@@ -1,9 +1,9 @@
 import { OpenAIChatMessage } from "@/utils/OpenAI";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdPerson, MdSmartToy, MdContentCopy, MdAutorenew } from "react-icons/md";
 import AssistantMessageContent from "./AssistantMessageContent";
 import UserMessageContent from "./UserMessageContent";
-import OpenAIContext, { useOpenAI } from "../context/OpenAIProvider";
+import { useOpenAI } from "../context/OpenAIProvider";
 
 type Props = {
   message: OpenAIChatMessage;
